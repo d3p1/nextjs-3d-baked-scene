0dc97c34e3590d9f550ad54f58f93aaede759586
C. M. de Picciotto <d3p1@d3p1.dev>
2025-11-01 21:37:16 +0000

    feat: add baked room [#1]

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,20 @@
  * @description Page
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
+'use client'
+
+import {Gltf, OrbitControls} from '@react-three/drei'
+import {Canvas} from '@react-three/fiber'
+
+const Room = () => {
+  return <Gltf src="/nextjs-3d-baked-scene/room.baked.glb" />
+}
+
 export default function Page() {
-  return <h1>HELLO WORLD</h1>
+  return (
+    <Canvas camera={{position: [1, 1, 1]}}>
+      <OrbitControls maxPolarAngle={Math.PI / 3} />
+      <Room />
+    </Canvas>
+  )
 }
