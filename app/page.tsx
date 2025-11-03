3428ea2e1ad3af8a4ab6639644181630d8ca58a0
C. M. de Picciotto <d3p1@d3p1.dev>
2025-11-03 04:53:27 +0000

    feat: add loader [#1]

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,30 @@
  */
 'use client'
 
-import {Gltf, OrbitControls} from '@react-three/drei'
+import {Html, OrbitControls, useGLTF} from '@react-three/drei'
 import {Canvas} from '@react-three/fiber'
+import {Suspense} from 'react'
+
+const Loader = () => {
+  return (
+    <Html center={true}>
+      <p>Loading...</p>
+    </Html>
+  )
+}
 
 const Room = () => {
-  return <Gltf src="/nextjs-3d-baked-scene/room.baked.glb" />
+  const model = useGLTF('/nextjs-3d-baked-scene/room.baked.glb')
+  return <primitive object={model.scene} />
 }
 
 export default function Page() {
   return (
     <Canvas camera={{position: [1, 1, 1]}}>
       <OrbitControls maxPolarAngle={Math.PI / 3} />
-      <Room />
+      <Suspense fallback={<Loader />}>
+        <Room />
+      </Suspense>
     </Canvas>
   )
 }
